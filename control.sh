059aba2a3ebb429b1e1bab5f6229eb32de9e9d4e
jamescarl96@gmail.com <jamescarl96@gmail.com>
2017-09-30 19:23:18 -0400

    Added a sandboxed launcher script for Linux/Unix systems. Changes to the bot token file are now ignored by git.

---
+++ control.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Only this user may control the Minecraft servers.
+TARGET_USER="discord-bots"
+
+if [ "$TARGET_USER" != "$USER" ]; then
+    echo "Only the user $TARGET_USER may run this script."
+    exit 1
+fi
+
+SESSION="discord-bot-`basename $PWD`"
+
+#COMMAND="bash"
+#COMMAND="--audit"
+COMMAND="python3 carlbot.py"
+SANDBOX="firejail --profile=/home/shared/sandbox-profiles/discord-bot.profile --whitelist=$PWD $COMMAND"
+
+start() {
+    echo "Session name: $SESSION"
+    echo "Launching from secured sandbox."
+    tmux new-session -d -s $SESSION $SANDBOX
+    tmux detach -s $SESSION
+}
+
+stop() {
+    echo "Stopping session: $SESSION"
+    tmux send-keys -t $SESSION 'shutdown' C-m
+}
+
+attach() {
+    echo "Attaching to session: $SESSION"
+    tmux attach-session -t $SESSION
+}
+
+case "$1" in
+start)
+    start
+;;
+stop)
+    stop
+;;
+attach)
+    attach
+;;
+restart)
+    stop
+    sleep 1
+    echo "Restarting bot..."
+    sleep 1
+    start
+;;
+*)
+    echo "Usage: %0 (start|stop|restart|attach)"
+;;
+esac
+
