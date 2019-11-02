096632d0e03369b5dc12319a083bcd3f6010a991
Kaynn Cahya <kaynncahya@outlook.com>
2019-11-02 13:35:29 +0900

    Added Minesweeper command

--- src/main/java/net/artifactgaming/carlbot/modules/games/Games.java
+++ src/main/java/net/artifactgaming/carlbot/modules/games/Games.java
@@ -2,10 +2,10 @@ package net.artifactgaming.carlbot.modules.games;
 
 import net.artifactgaming.carlbot.*;
 import net.artifactgaming.carlbot.Module;
+import net.artifactgaming.carlbot.modules.games.minesweeper.MinesweeperGenerator;
+import net.artifactgaming.carlbot.modules.games.minesweeper.MinesweeperTile;
 import net.artifactgaming.carlbot.modules.selfdocumentation.Documented;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-
-import java.util.Collection;
 import java.util.List;
 
 public class Games implements Module, Documented {
@@ -15,14 +15,59 @@ public class Games implements Module, Documented {
 
     }
 
+    private class MinesweeperCommand implements Command, Documented {
+
+        @Override
+        public String getCallsign() {
+            return "minesweeper";
+        }
+
+        @Override
+        public void runCommand(MessageReceivedEvent event, java.lang.String rawString, List<java.lang.String> tokens) throws Exception {
+            MinesweeperTile[][] playField = MinesweeperGenerator.generateField();
+
+            StringBuilder playFieldAsString = new StringBuilder();
+
+            for (int row = 0; row < MinesweeperGenerator.rows; ++row){
+                for (int column = 0; column < MinesweeperGenerator.columns; ++column){
+                    // TODO: Use Emote + Spoiler
+                    if (playField[row][column].isSafe()){
+                        playFieldAsString.append(playField[row][column].getSurroundingMinesCount());
+                    } else {
+                        playFieldAsString.append('B');
+                    }
+                }
+                playFieldAsString.append(Utils.NEWLINE);
+            }
+
+            event.getChannel().sendMessage(playFieldAsString.toString()).queue();
+        }
+
+        @Override
+        public Module getParentModule() {
+            return Games.this;
+        }
+
+        @Override
+        public String getDocumentation() {
+            return "Create a playing field for minesweeper (10 x 10)";
+        }
+
+        @Override
+        public String getDocumentationCallsign() {
+            return "minesweeper";
+        }
+    }
+
     private class GamesCommands implements Command {
 
         private CommandHandler commands;
 
         GamesCommands(CarlBot carlbot) {
             commands = new CommandHandler(carlbot);
-
             commands.setSubName(this.getCallsign());
+
+            commands.addCommand(new MinesweeperCommand());
         }
 
         @Override
