22c752f6afea7f08e8e5489a112cf553b0dac00a
Kaynn Cahya <kaynncahya@outlook.com>
2019-11-02 13:46:14 +0900

    Updated Mine generation algorithm

--- src/main/java/net/artifactgaming/carlbot/modules/games/minesweeper/MinesweeperGenerator.java
+++ src/main/java/net/artifactgaming/carlbot/modules/games/minesweeper/MinesweeperGenerator.java
@@ -1,55 +1,26 @@
 package net.artifactgaming.carlbot.modules.games.minesweeper;
 
-import java.util.function.BooleanSupplier;
-
 public class MinesweeperGenerator {
     public static final int rows = 10;
     public static final int columns = 10;
 
-    private static final int maximumMinesCount = 12;
-
-    /**
-     * Increasing this value will increase the chance of mines being generated on the next go.
-     * (Increases the chance of a mine being generated on the next tile, if the previous tile had no mine)
-     */
-    private static final int mineGenerationBias = 5;
-
-    /**
-     * Generated number needs to exceed this value in order to generate a mine.
-     * (Out of 100)
-     */
-    private static final int mineGenerationCap = 60;
+    private static final int guaranteedMinesCount = 14;
 
     public static MinesweeperTile[][] generateField(){
-        int minesCreated = 0;
-        final int[] currentMineBias = {0};
-
-        ///region Local_Function
-        BooleanSupplier willHaveMineOnCurrentTile = () -> {
-            if (minesCreated >= maximumMinesCount){
-                return false;
-            }
-
-            int chanceToGenerateMine = (int)((Math.random() * 100 + 1) + currentMineBias[0]);
-
-            if (chanceToGenerateMine >= mineGenerationCap){
-                currentMineBias[0] = 0;
-                return true;
-            } else {
-                currentMineBias[0] += mineGenerationBias;
-                return false;
-            }
-        };
-        ///endregion
 
         MinesweeperTile[][] playField = createEmptyField();
 
-        for (int x = 0; x < rows; ++x){
-            for (int y = 0; y < columns; ++y){
-                if (willHaveMineOnCurrentTile.getAsBoolean()){
-                    UpdateTileWithMine(playField, x, y);
-                }
-            }
+        for (int mineCreated = 0; mineCreated < guaranteedMinesCount; ++ mineCreated){
+            int xPosToPlaceMine;
+            int yPosToPlaceMine;
+            // Generate a new position to place a mine.
+            // (ensure that the position does not have a mine currently.)
+            do {
+                xPosToPlaceMine = (int) (Math.random() * rows);
+                yPosToPlaceMine = (int) (Math.random() * columns);
+            } while (!playField[xPosToPlaceMine][yPosToPlaceMine].isSafe());
+
+            UpdateTileWithMine(playField, xPosToPlaceMine, yPosToPlaceMine);
         }
 
         return playField;
@@ -66,6 +37,8 @@ public class MinesweeperGenerator {
 
                 // Ignore self or out-of-bounds.
                 if (currentX < 0 || currentY < 0 || (currentY == y && currentX == x) || currentX >= rows || currentY >= columns ){
+                    continue;
+                } else {
                     // Increase mine count on surrounding tile.
                     int newMineCount = tiles[currentX][currentY].getSurroundingMinesCount() + 1;
                     tiles[currentX][currentY].setSurroundingMinesCount(newMineCount);
